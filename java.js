0c8535dd9fb00bb612f044c2f856fa4ca0f5bfcc
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-04-04 15:44:21 -0500

    got 16x16 grid

--- java.js
+++ java.js
@@ -1,12 +1,34 @@
 
 let container = document.querySelector("#container");
 let stbtn = document.querySelector("#buttonStart");
-const div = document.createElement('div');
+const div1 = document.createElement('div');
+const div2 = document.createElement('div');
+const div3 = document.createElement('div');
+const div4 = document.createElement('div');
 
-    
+const div1a = document.createElement('div');
+const div1b = document.createElement('div');
+const div1c = document.createElement('div');
+const div1d = document.createElement('div');
 
+const div2a = document.createElement('div');
+const div2b = document.createElement('div');
+const div2c = document.createElement('div');
+const div2d = document.createElement('div');
 
+const div3a = document.createElement('div');
+const div3b = document.createElement('div');
+const div3c = document.createElement('div');
+const div3d = document.createElement('div');
 
+const div4a = document.createElement('div');
+const div4b = document.createElement('div');
+const div4c = document.createElement('div');
+const div4d = document.createElement('div');
 
-    container.append(div, div, div, div);
-    div.innerText = "start";
+container.append(div1,div2,div3,div4);
+
+div1.append(div1a,div1b,div1c,div1d);
+div2.append(div2a,div2b,div2c,div2d);
+div3.append(div3a,div3b,div3c,div3d);
+div4.append(div4a,div4b,div4c,div4d);
