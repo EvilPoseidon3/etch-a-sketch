618bd863a5973b1da7bb8d5e6ece2400cdbe915e
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-04-04 12:41:45 -0500

    initial setup

---
+++ java.js
@@ -0,0 +1,12 @@
+
+let container = document.querySelector("#container");
+let stbtn = document.querySelector("#buttonStart");
+const div = document.createElement('div');
+
+    
+
+
+
+
+    container.append(div, div, div, div);
+    div.innerText = "start";
