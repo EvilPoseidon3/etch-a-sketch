726680818399f2f70d962b71b2df8280ab7908ff
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-04-04 19:23:11 -0500

    got function to mk grid working

--- java.js
+++ java.js
@@ -1,34 +1,35 @@
 
 let container = document.querySelector("#container");
 let stbtn = document.querySelector("#buttonStart");
-const div1 = document.createElement('div');
-const div2 = document.createElement('div');
-const div3 = document.createElement('div');
-const div4 = document.createElement('div');
 
-const div1a = document.createElement('div');
-const div1b = document.createElement('div');
-const div1c = document.createElement('div');
-const div1d = document.createElement('div');
 
-const div2a = document.createElement('div');
-const div2b = document.createElement('div');
-const div2c = document.createElement('div');
-const div2d = document.createElement('div');
 
-const div3a = document.createElement('div');
-const div3b = document.createElement('div');
-const div3c = document.createElement('div');
-const div3d = document.createElement('div');
 
-const div4a = document.createElement('div');
-const div4b = document.createElement('div');
-const div4c = document.createElement('div');
-const div4d = document.createElement('div');
 
-container.append(div1,div2,div3,div4);
 
-div1.append(div1a,div1b,div1c,div1d);
-div2.append(div2a,div2b,div2c,div2d);
-div3.append(div3a,div3b,div3c,div3d);
-div4.append(div4a,div4b,div4c,div4d);
+
+function makeGrid(gridWidth) {
+    
+    for (let i = 0; i < gridWidth ; i++) {
+        let number1 = Math.random() * 100;
+        let number2 = Math.random() * 100;
+        let randomnumber = number1 * number2
+        randomnumber = document.createElement("div")
+        container.append(randomnumber)
+
+        
+
+        for (let i = 0; i < gridWidth ; i++) {
+            let number1a = Math.random() * 100;
+            let number2a = Math.random() * 100;
+            let randomnumber2 = number1a * number2a
+            randomnumber2 = document.createElement("div")
+            randomnumber.append(randomnumber2)
+
+        }
+    }
+
+    
+}
+
+console.log(makeGrid(10))
