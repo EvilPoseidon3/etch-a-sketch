423a183975a998d3a70081fa19cb8c4bf2e5a2c9
EvilPoseidon3 <160077853+EvilPoseidon3@users.noreply.github.com>
2024-04-04 22:10:50 -0500

    paused for the night 4/4

--- java.js
+++ java.js
@@ -2,15 +2,27 @@
 let container = document.querySelector("#container");
 let stbtn = document.querySelector("#buttonStart");
 
-
+stbtn.addEventListener("click", () => {
+    let gridSize = prompt("what size would you like the Etch-a-Sketch?", "5-100");
+    
+    let gridDivP = document.querySelectorAll(".gridDivParent");
+    let gridDivC = document.querySelectorAll(".gridDivChild")
+    if (container.hasChildNodes()) {
+    container.removeChild(container.children[0])};
+        console.log(container.hasChildNodes());
+    makeGrid(gridSize);
+})
 
 function makeGrid(gridWidth) {
+
     
+
     for (let i = 0; i < gridWidth ; i++) {
         let number1 = Math.random() * 100;
         let number2 = Math.random() * 100;
         let randomnumber = number1 * number2
         randomnumber = document.createElement("div")
+        randomnumber.setAttribute("class", "gridDivParent")
         container.append(randomnumber)
 
         
@@ -20,6 +32,7 @@ function makeGrid(gridWidth) {
             let number2a = Math.random() * 100;
             let randomnumber2 = number1a * number2a
             randomnumber2 = document.createElement("div")
+            randomnumber2.setAttribute("class", "gridDivChild")
             randomnumber.append(randomnumber2)
 
             randomnumber2.addEventListener("mouseover", () => {
@@ -33,4 +46,4 @@ function makeGrid(gridWidth) {
 
 
 
-makeGrid(100)
+// makeGrid(100)
